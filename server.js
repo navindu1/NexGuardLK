2af4ab535bd782a9679e25bd3fed2042cc55ec2f
NavinduR <110601389+navindu1@users.noreply.github.com>
2025-09-11 18:41:01 +0530

    comit 10

--- server.js
+++ server.js
@@ -1255,7 +1255,7 @@ app.post(
   }
 );
 
-app.get('*', (req, res) => {
+app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 // ---------------------------------------------------
