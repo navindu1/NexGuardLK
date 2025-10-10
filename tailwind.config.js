d8e89bdf3a7f1cfacdc0e95967520fd0f31f4b1e
NavinduR <110601389+navindu1@users.noreply.github.com>
2025-10-11 01:49:25 +0530

    ne

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,20 @@
 /** @type {import('tailwindcss').Config} */
+
+
 module.exports = {
-  content: [
-    './public/**/*.html',
-    './public/**/*.js',
-  ],
+  content: ["./public/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'custom-black': '#000000',
+        'custom-gray': {
+          DEFAULT: '#a2a2a2',
+          light: '#c4c4c4',
+          dark: '#808080',
+        },
+        'accent': '#a2a2a2', // You can change this to a different accent color if you like
+      },
+    },
   },
   plugins: [],
-}  
+}
